270cfd8581fbd8586b35aa6bf3c33d269f76beeb
martimfm1 <158637093+NotTryMitO@users.noreply.github.com>
2025-03-16 23:34:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -20,17 +20,15 @@ intents.presences = True
 intents.reactions = True
 intents.integrations = True
 
-bot1_token = "MTIyMzY2MDY5NjU4NDA2NTE4Nw.GsxnP1._lgKJdoAmSJQQ6I5_3M7xQhcWyRVtublFCfc0Q"
-bot2_token = "MTMwMzcyODMyOTY4OTM5OTI5Nw.GSiGuC.AVGg_dWqOm6EQz1TU179Wwg5tdXvJDRKdmltE0"
-bot3_token = "MTIxOTc0MjAyODI0NjgxNDgxMQ.GhscDu._Ey7uAtEC9JNN2Q5k5C6H8k-D9Ex52wMkH6LnM"
-bot4_token = "MTMzNjgxNTY4NTY5MDU4OTI0NQ.GCns2E.jDf6a170GWczvnhIzAmwGzuDWj_zloTcuo95sI"
-bot5_token = "MTMzMzEyMjIyMTA5NDQ2OTY4Mg.GBx0gN.t71b88N2kYHlBDgrAQTkXHKlGgxkcl4MxFf7uo"
-bot6_token = 'MTMwODUyMjM2NDcyNzg1MzA5Ng.GA63xU.JKH71YJxHRW6N7V193D-imew3hYMR7VqRbiuKo'
+bot1_token = '' #NTM BOT DEV
+bot2_token = '' #NTM TICKET
+bot3_token = '' #NTM MODS BOT
+bot4_token = '' #GK COMMUNITY
+bot6_token = '' #+SHOP
 bot1 = commands.Bot(command_prefix="001", intents=intents) #NTM BOT DEV
 bot2 = commands.Bot(command_prefix="22", intents=intents) #NTM TICKET
 bot3 = commands.Bot(command_prefix='0001', intents=intents) #NTM MODS BOT
 bot4 = commands.Bot(command_prefix='0001', intents=intents,) #GK Community
-bot5 = commands.Bot(command_prefix="ds", intents=intents) #62Degrees
 bot6 = commands.Bot(command_prefix="0001", intents=intents) #+SHOP
 
 #NTM MODS BOT
@@ -564,246 +562,6 @@ async def on_ready():
         print(f'Failed to sync commands: {e}')
     await bot4.change_presence(activity=activity)
 
-'''62Degrees'''
-
-@bot5.event
-async def on_ready():
-    activity = discord.Activity(type=discord.ActivityType.watching, name="62Degrees")
-    print(f'Logged in as {bot5.user}')
-    
-    try:
-        synced = await bot5.tree.sync()
-        print(f'{bot5.user} synced {len(synced)} command(s)')
-    except discord.HTTPException as e:
-        print(f'Failed to sync commands: {e}')
-    
-    await bot5.change_presence(activity=activity)
-
-    if not send_dscrack.is_running():
-        send_dscrack.start()
-    
-    if not terms_loop.is_running():
-        terms_loop.start()
-
-class Dropdown(discord.ui.Select):
-    def __init__(self):
-        options = [
-            discord.SelectOption(value="shopping", label="Shopping", emoji="🛒", description="Used to buy something"),
-            discord.SelectOption(value="support", label="Support", emoji="💳", description="Used to help with problems you have"),
-            discord.SelectOption(value="doubts", label="Doubts", emoji="❔", description="Used to help with your doubts")
-        ]
-        super().__init__(
-            placeholder="Select an option...",
-            min_values=1,
-            max_values=1,
-            options=options,
-            custom_id="persistent_view:dropdown_help"
-        )
-
-    async def callback(self, interaction: discord.Interaction):
-        if self.values[0] in ["shopping", "support", "doubts"]:
-            selected_option = self.values[0]
-            await interaction.response.defer()
-            view = discord.ui.View()
-            open_button = TicketButton(label=f"Open {selected_option} Ticket", ticket_option=selected_option)
-            view.add_item(open_button)
-            await interaction.followup.send(content=f"Press the button below to open a {selected_option} ticket", view=view, ephemeral=True)
-
-class PersistentView(discord.ui.View):
-    def __init__(self):
-        super().__init__(timeout=None)
-        self.add_item(Dropdown())
-
-class TicketButton(discord.ui.Button):
-    def __init__(self, label, ticket_option=None, image_url=None, ticket_text=None):
-        super().__init__(style=discord.ButtonStyle.grey, label=label)
-        self.ticket_option = ticket_option
-        self.image_url = image_url
-        self.ticket_text = ticket_text
-
-    async def callback(self, interaction: discord.Interaction):
-        if self.ticket_option:
-            guild = interaction.guild
-            user = interaction.user
-
-            existing_numbers = set()
-            for channel in guild.text_channels:
-                if channel.name.startswith(tuple(f"{i} - " for i in range(1, 101))):
-                    try:
-                        number_part = int(channel.name.split(" - ")[0])
-                        existing_numbers.add(number_part)
-                    except ValueError:
-                        continue
-
-            ticket_number = next((i for i in range(1, 101) if i not in existing_numbers), None)
-
-            if ticket_number is None:
-                await interaction.response.send_message("O número máximo de tickets foi atingido!", ephemeral=True)
-                return
-
-            ticket_name = f"{self.ticket_option} | {user.name}"
-
-            role_to_mention = guild.get_role(1333186656416825393)
-            role_mention = role_to_mention.mention if role_to_mention else "@everyone"
-
-            overwrites = {
-                guild.default_role: discord.PermissionOverwrite(read_messages=False),
-                guild.me: discord.PermissionOverwrite(read_messages=True),
-                user: discord.PermissionOverwrite(read_messages=True, send_messages=True),
-            }
-            
-            if role_to_mention:
-                overwrites[role_to_mention] = discord.PermissionOverwrite(read_messages=True, send_messages=True)
-
-            ticket_channel = await guild.create_text_channel(ticket_name, overwrites=overwrites)
-
-            ticket_message = f"📩 **|** Hi {user.mention}! You opened the ticket {self.ticket_option}. Send all possible information about your case and wait until {role_mention} replies."
-            if self.ticket_text:
-                ticket_message += f"\n\n{self.ticket_text}"
-            if self.image_url:
-                ticket_message += f"\n{self.image_url}"
-            
-            await ticket_channel.send(ticket_message)
-            await interaction.response.send_message(f"You opened the ticket {self.ticket_option} in {ticket_channel.mention}", ephemeral=True)
-
-            # Botão para fechar o ticket
-            view = discord.ui.View()
-            close_button = CloseButton("Close", ticket_channel.id)
-            view.add_item(close_button)
-            await ticket_channel.send("Click the button below to close this ticket:", view=view)
-        else:
-            await interaction.response.send_message("Invalid ticket option.", ephemeral=True)
-
-class CloseButton(discord.ui.Button):
-    def __init__(self, label, channel_id):
-        super().__init__(style=discord.ButtonStyle.grey, label=label)
-        self.channel_id = channel_id
-
-    async def callback(self, interaction: discord.Interaction):
-        ticket_channel = interaction.guild.get_channel(self.channel_id)
-        if ticket_channel:
-            await ticket_channel.delete()
-            await interaction.response.send_message("The ticket was closed successfully.", ephemeral=True)
-        else:
-            await interaction.response.send_message("Unable to find ticket channel.", ephemeral=True)
-
-@tasks.loop(minutes=10)
-async def send_dscrack():
-    channel = bot5.get_channel(1333186657028935763)
-    if channel:
-        try:
-            await channel.purge(limit=None)
-            print("Channel messages purged successfully.")
-
-            embed = discord.Embed(
-                title="**SERVICE**",
-                description="➡️ - To open a ticket, select one of the options below:",
-                color=0xffffff
-            )
-            embed.add_field(name="", value="```ㅤㅤㅤㅤ! 𝘽𝙀𝙁𝙊𝙍𝙀 𝙊𝙋𝙀𝙉𝙄𝙉𝙂 !ㅤㅤㅤ```", inline=False)
-            embed.add_field(name="", value="➡️ - Do not open a ticket without **NECESSITY**", inline=False)
-            embed.add_field(name="", value="➡️ - Don't tag the MODERATORS, they are aware of your ticket", inline=False)
-            embed.set_image(url="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMnU2Ymx4MGp0MW50cDNtcWExcGw2cW9oZTBkcGQ2MjdhYTN6YjVhbiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qSZEBFDK9yGWoyIvn4/giphy.gif")
-
-            await channel.send(embed=embed, view=PersistentView())
-            print("Successfully sent the embed message.")
-        
-        except Exception as e:
-            print(f"Error in send_dscrack task: {e}")
-
-@bot5.tree.command(name="suggestion", description="Do a suggestion in the suggestions channel!")
-@app_commands.describe(texto="The suggestion you want to send")
-async def suggestion(interaction: discord.Interaction, texto: str):
-    CANAL_PERMITIDO_ID = 1334297450139095082
-
-    if interaction.channel_id != CANAL_PERMITIDO_ID:
-        await interaction.response.send_message(
-            "This command can only be used in the allowed channel.", ephemeral=True
-        )
-        return
-
-    embed = discord.Embed(
-        title="Suggestion",
-        description=texto,
-        color=discord.Color.yellow()
-    )
-
-    if interaction.user:
-        user = interaction.user
-        embed.set_author(name=user.display_name, icon_url=user.avatar.url if user.avatar else None)
-
-    await interaction.channel.send(embed=embed)
-
-@bot5.tree.command(name='website', description='Official website of 62degrees')
-async def website(interaction:discord.Interaction):
-    await interaction.response.send_message('https://62degrees.me')
-
-class RoleButton(discord.ui.Button):
-    def __init__(self):
-        super().__init__(label="Accept Terms", style=discord.ButtonStyle.green, emoji="✅")
-
-    async def callback(self, interaction: discord.Interaction):
-            guild = interaction.guild
-            role = guild.get_role(1333390169499242556)
-            await interaction.user.add_roles(role)
-            
-            await interaction.response.send_message("🎉 You accepted the terms and got the role!", ephemeral=True)
-
-            print(f"Role '{role.name}' given to {interaction.user.name} in {guild.name}.")
-
-class TermsView(discord.ui.View):
-    def __init__(self):
-        super().__init__(timeout=None)
-        self.add_item(RoleButton())
-
-@tasks.loop(minutes=5)
-async def terms_loop():
-    channel = bot5.get_channel(1333186656848707669)
-    if channel:
-        try:
-            await channel.purge(limit=None)
-            print("Canal dos termos apagado")
-
-            embed = discord.Embed(
-                title="Terms of Service",
-                description=(
-                    "**Be respectful and civilized.**\n"
-                    "• No type of harassment, sexism, racism, homophobia, xenophobia or hate speech will be tolerated. "
-                    "If you see someone breaking the rules, notify the staff immediately!\n\n"
-
-                    "**Do not send adult or questionable content.**\n"
-                    "• It is prohibited to send text, images or links with content that contains nudity, sex, violence, "
-                    "graphically disturbing content, political or religious subjects.\n\n"
-
-                    "**Don't spam or flood.**\n"
-                    "• Do not spam or flood text, images, links, emojis, etc. Do not send several messages in a row "
-                    "or messages that do not contribute anything to the discussion.\n\n"
-
-                    "**Don't advertise.**\n"
-                    "• Self-promotion (server invites, advertisements, etc.) without permission from a team member is prohibited. "
-                    "This includes statuses and sending DM's to other members.\n\n"
-
-                    "**Use channels correctly.**\n"
-                    "• We have several channels, use each one appropriately. If you have a problem on Discord or in-game, "
-                    "open a ticket at https://discord.com/channels/1333186656416825387/1333186657028935763\n\n"
-
-                    "**Report other users who break the rules.**\n"
-                    "• If you see something that breaks the rules or something that makes you feel unsafe, let the team know."
-                ),
-                color=0xffffff
-            )
-            embed.set_footer(
-                text="™ 62degrees © All rights reserved", 
-                icon_url="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbnc5YnBkenoyeHpqcnEya2pwMzY5amt0YTdyeGpvbjE1cHpqMHQwbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qSZEBFDK9yGWoyIvn4/giphy.gif"
-                )   
-            
-            await channel.send(embed=embed, view=TermsView())
-            print("Regras enviadas com sucesso")
-
-        except Exception as e:
-            print(f"Erro ao apagar o canal ou enviar as regras: {e}")
-
-
 '''+SHOP'''
 @bot6.event
 async def on_ready():
@@ -1142,8 +900,7 @@ async def main():
         bot2.start(bot2_token),
         bot3.start(bot3_token),
         bot4.start(bot4_token),
-        bot5.start(bot5_token),
-        bot6.start(bot6_token),
+        bot5.start(bot6_token),
     )
 
 if __name__ == "__main__":
